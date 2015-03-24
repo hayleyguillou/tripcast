21efb1e39fdd109eeb7f23a22de8cd43cd9efbec
brianyeo <byeo999@gmail.com>
2015-03-24 13:14:14 -0500

    new trip n shit
    sacramento to green bay is now available
    you have to actually type the route in
    fixed colours
    refactored a bunch

---
+++ app/src/main/java/com/comp4020/tripcast/Trip.java
@@ -0,0 +1,39 @@
+package com.comp4020.tripcast;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
+import android.app.Activity;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by brianyeo on 2015-03-23.
+ */
+
+//Seattle to St Louis
+public class Trip {
+
+    public static void newTrip(int whichTrip, GoogleMap map, int theRoute, int progress){
+        if (whichTrip == 1) {
+            Trip1.displayWeather1(map, theRoute, progress);
+        }
+        else if (whichTrip ==2) {
+            Trip2.displayWeather(map, theRoute, progress);
+        }
+        else {
+
+        }
+    }
+
+ }
